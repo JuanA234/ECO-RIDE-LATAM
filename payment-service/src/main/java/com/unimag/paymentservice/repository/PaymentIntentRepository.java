c8477ce9e8c083279e5bd79e1649c25e89366444
Juan A <juanandreslujan30@gmail.com>
2025-11-22 20:37:50 -0500

    agregado filtros y predicados custom

--- payment-service/src/main/java/com/unimag/paymentservice/repository/PaymentIntentRepository.java
+++ payment-service/src/main/java/com/unimag/paymentservice/repository/PaymentIntentRepository.java
@@ -2,6 +2,7 @@ package com.unimag.paymentservice.repository;
 
 import com.unimag.paymentservice.entity.PaymentIntent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface PaymentIntentRepository extends JpaRepository<PaymentIntent, Long> {
+public interface PaymentIntentRepository extends ReactiveCrudRepository<PaymentIntent, Long> {
 }
