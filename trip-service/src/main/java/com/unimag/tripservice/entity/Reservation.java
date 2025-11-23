208b0783e9ab41d0dec0309f5532dea29b47654e
Juan A <juanandreslujan30@gmail.com>
2025-11-22 23:18:15 -0500

    api gateway terminado y dependecias para programacion reactiva

--- trip-service/src/main/java/com/unimag/tripservice/entity/Reservation.java
+++ trip-service/src/main/java/com/unimag/tripservice/entity/Reservation.java
@@ -1,16 +1,16 @@
 package com.unimag.tripservice.entity;
 
 import com.unimag.tripservice.enums.ReservationStatus;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
 @Data
-@Entity
 @Table(name = "reservations")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,15 +18,9 @@ import java.time.LocalDateTime;
 public class Reservation {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
-    @JoinColumn(name = "trip_id", nullable = false)
     private Trip trip;
-    @Column(name = "passenger_id")
     private Long passengerId;
-    @Enumerated(EnumType.STRING)
     private ReservationStatus status;
-    @Column(name = "created_at")
     private LocalDateTime createdAt;
 }
--- trip-service/src/main/java/com/unimag/tripservice/entity/Trip.java
+++ trip-service/src/main/java/com/unimag/tripservice/entity/Trip.java
@@ -1,11 +1,12 @@
 package com.unimag.tripservice.entity;
 
 import com.unimag.tripservice.enums.TripStatus;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -16,30 +17,21 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
 @Table(name = "trips")
 @Builder
 public class Trip {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "driver_id")
     private Long driverId;
 
     private String origin;
 
     private String destination;
 
-    @Column(name = "start_time")
     private LocalDateTime startTime;
-    @Column(name = "seats_total")
     private Integer seatsTotal;
-    @Column(name = "seats_available")
     private Integer seatsAvailable;
     private BigDecimal price;
-    @Enumerated(EnumType.STRING)
     private TripStatus status;
-    @OneToMany(mappedBy = "trip", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Reservation> reservations = new ArrayList<>();
 }
