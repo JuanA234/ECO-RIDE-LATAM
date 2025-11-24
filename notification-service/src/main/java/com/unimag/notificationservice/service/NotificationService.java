2f0fbf1f66931b91f899324f73ddb83640b75cc8
arturoandresv <arturovelasquezo0711@gmail.com>
2025-11-23 21:23:00 -0500

    delete files

--- notification-service/src/main/java/com/unimag/notificationservice/service/NotificationService.java
+++
@@ -1,8 +0,0 @@
-package com.unimag.notificationservice.service;
-
-import com.unimag.notificationservice.dto.request.SendNotificationRequestDTO;
-import com.unimag.notificationservice.dto.response.OutboxResponseDTO;
-
-public interface NotificationService {
-    OutboxResponseDTO send(SendNotificationRequestDTO sendNotificationRequestDTO);
-}
